b9af77a2053c9236b50762f6afef1635f7e7329f
Darenz Jasper Hicap <hicap.darenzjasper@gmail.com>
2025-02-18 11:29:07 +0800

    refactor Card component for improved layout and styling consistency

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,22 +15,24 @@ const CustomCard = ({
 }: CardProps & { children?: React.ReactNode }) => {
   return (
     <Card
-      className="grid justify-items-center items-center text-center p-1 rounded-md !mix-blend-hard-light"
+      className="grid justify-items-center content-between items-center p-2 rounded-md !mix-blend-hard-light hover:!shadow-lg"
       sx={{
+        gridTemplateRows: "auto 1fr",
         backgroundColor: theme === "dark" ? "#2c3a33a8" : "#f0f0f0",
       }}
     >
       <StyledImageComponent
         src={imgSrc}
         alt={title}
-        className="!rounded-md p-2"
+        className="!rounded-md p-2 row-span-1"
       />
       <Typography
         variant="subtitle1"
+        className="row-span-1"
         sx={{
-          width: "15ch",
+          textAlign: "center",
+          width: "12ch",
           wordWrap: "break-word",
-          marginTop: 1,
           color: theme === "dark" ? "#fff" : "text.primary",
         }}
       >
